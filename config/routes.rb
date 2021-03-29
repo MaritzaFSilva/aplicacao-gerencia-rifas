98e45dfca400bb2fd02bddac0415167abcd09d21
MaritzaFSilva <maritzafer98@gmail.com>
2021-03-29 19:46:31 -0300

    route to root

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   resources :users
   resources :types
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'types#index'
 end
